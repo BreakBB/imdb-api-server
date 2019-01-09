fc0f66fe2024fc049dd62884325caf4c4efef79f
BreakBB <b.boeing@online.de>
2019-01-09 21:54:01 +0100

    Add support for multiple API keys

--- src/imdbQuery.js
+++ src/imdbQuery.js
@@ -3,11 +3,13 @@ const imdb = require('imdb-api');
 const fs = require("fs");
 
 console.log("Trying to load api.key");
-const apiKey = fs.readFileSync("api.key", "utf-8");
-if (apiKey === "") {
+const apiKeys = fs.readFileSync("api.key", "utf-8").split(/\r?\n/);
+if (apiKeys.length === 0) {
   console.error("Couldn't load api.key");
   throw "api.key file not found";
 }
+let countAPIKey = 0;
+let apiKey = apiKeys[countAPIKey];
 
 module.exports = {
   getData: async function getData(title, res) {
@@ -23,11 +25,24 @@ module.exports = {
         {"name": title},
         {"apiKey": apiKey, "timeout": 30000});
 
-      console.log("Found data entry for: ", title);
+      console.log("Found data entry for:", title);
       return result;
     }
     catch (err) {
-      console.error("Couldn't find data entry for: ", title);
+      console.error("Couldn't find data entry for:", title);
+
+      if (err.statusCode === 401) {
+        if (apiKeys.length > countAPIKey) {
+          console.log("Request limit reached. Changing API key");
+          countAPIKey += 1;
+          apiKey = apiKeys[countAPIKey];
+        }
+        else {
+          console.log("Request limit reached. Out of API keys");
+          process.exit();
+        }
+      }
+
       // result is "err"
       res.status(404);
       res.json(err);
